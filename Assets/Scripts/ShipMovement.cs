64a001fd64afdc03302a7a314351082b956de3b3
Shargon <Splyth@gmail.com>
2018-03-05 20:56:07 -0600

    Name Update

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -16,8 +16,6 @@ public int invert = -1; //Negative 1 for invert, positive 1 for not
 		Vector3 finalDirection = new Vector3(horizontal,invert*vertical,1.0f);
 		
 		transform.position += direction*movementSpeed*Time.deltaTime;
-		
-
 		transform.rotation = Quaternion.RotateTowards(transform.rotation,Quaternion.LookRotation(finalDirection),Mathf.Deg2Rad*50.0f);
 	}
 }
